212d97019b6776404b889aec25392a4a48cc7cf8
cupcakezzz <41251346+cupcakezzz@users.noreply.github.com>
2018-07-25 11:10:47 -0700

    Update jennifer.js

--- assets/js/jennifer.js
+++ assets/js/jennifer.js
@@ -1,3 +1,19 @@
+function pow(x, n) {
+  let result = x;
 
-let name = prompt('What is your name?', 'Jennifer');
-alert(`Hello and welcome to my page, ${name}!`);
+  for (let i = 1; i < n; ++i) {
+    result *= x;
+  }
+
+  return result;
+}
+
+let x = prompt("x?", '');
+let n = prompt("n?", '');
+
+if (n < 1) {
+  alert(`Power ${n} is not supported,
+    use an integer greater than 0`);
+} else {
+  alert( pow(x, n) );
+}
